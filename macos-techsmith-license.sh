80c9d661807ef5ecdd29e29e63dd87368bd147ef
ojablkach <vjnikon@mac.com>
2022-11-02 11:46:19 +0100

    add macos-techsmith-license

---
+++ macos-techsmith-license.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# script made by https://ojablkach.xyz
+# example
+# key 00000-ZZZZZ-YYYYY-XXXXX-JJJJJ
+# software Snagit
+# year 2023
+
+file="LicenseKey"
+key=$4
+software=$5
+year=$6
+
+if [[ -f "/Users/Shared/TechSmith/${software}/${file}" ]]
+	then
+		echo "The ${file} does exist!"
+	else
+		echo "The ${file} does not exist"
+		mkdir -p /Users/Shared/TechSmith/${software}
+		echo ${key} > /Users/Shared/TechSmith/${software}/${file}
+		echo "${file} installed"
+fi
